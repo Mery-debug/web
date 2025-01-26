d2449b9334d2fbfde689141471a2b1a94a480e32
Mery-debug <m.a.shap58000@gmail.com>
2025-01-26 06:18:09 +0300

    allmost

---
+++ main.py
@@ -0,0 +1,22 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+hostName = "localhost"
+serverPort = 8080
+
+
+def point_start(server_class=HTTPServer, handler_class=MyServer, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Serving on port {port}')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        httpd.server_close()
+        print("Server stopped")
+
+
+if __name__ == "__main__":
+    point_start()
